b7768d18a83f9d28af67f963c466c2a927837628
Geir Sjurseth <geir.sjurseth@gmail.com>
2016-12-27 18:44:32 +0100

    moved to a peerdep approach for aws-sdk. Added new mergeConfigs method

--- index.js
+++ index.js
@@ -3,8 +3,68 @@ const
   _         = require('lodash'),
   aws       = require('aws-sdk');
 
-//exports.config = (configName, configTable, region) => {
-exports.config = (p) => {
+// Use bluebird implementation of Promise
+aws.config.setPromisesDependency(require('bluebird'));
+
+const self = exports;
+
+exports.defaults = {};
+
+exports.parseArgs = list => {
+  let _cf = [];
+  list.forEach( i => {
+    if (typeof(i) === 'string') {
+      if ( _.has(self.defaults, 'tableName') ) {
+        _cf.push({
+          configName: i,
+          tableName: self.defaults.tableName,
+          region: self.defaults.region ? self.defaults.region : 'eu-west-1',
+          debug: self.defaults.debug ? self.defaults.debug : false
+        });
+      }
+      else {
+        throw new Error("tableName not defined. Have only configName: " + i);
+      }
+    }
+    else {
+      if ( _.has(i,'configName') ) {
+        if ( (_.has(self.defaults, 'tableName')) || (_.has(i,'tableName')) ) {
+          _cf.push({
+            configName: i.configName,
+            tableName: self.defaults.tableName,
+            region: self.defaults.region ? self.defaults.region : 'eu-west-1',
+            debug: self.defaults.debug ? self.defaults.debug : false
+          });
+        }
+        else {
+          throw new Error("tableName not defined. Have only configName: " + i.configName);
+        }
+      }
+    }
+  });
+  return _cf;
+}
+
+/*
+ * @params {array} list of either strings (setDefaults previously called) or config objects
+ * @returns {object} an object of configName: {configs}
+ */
+exports.mergeConfigs = list => {
+  return Promise.map(self.parseArgs(list), self.config)
+  .then( cf => {
+    let config = {};
+    cf.forEach( i => {
+      config[i.configName] = i.config;
+    });
+    return config;
+  });
+}
+
+exports.setDefaults = o => {
+  _.assign(self.defaults, o);
+}
+
+exports.config = p => {
   debug = false;
   if ( _.has(p, 'debug') ) {
     if (p.debug) {
@@ -12,23 +72,26 @@ exports.config = (p) => {
     }
   }
   if ( _.has(p,'region') ) {
-    aws.config.region = p.region;
+    aws.config.update({region: p.region});
   }
 
   if (debug) {
     console.info('--## Trying config %s from table: %s in region: %s ##--', p.configName,p.tableNam,p.region);
   }
 
-  var dynDB   = Promise.promisifyAll(new aws.DynamoDB.DocumentClient());
-  var params = {
+  let params = {
     TableName: p.tableName,
     Key: { configName: p.configName }
   };
 
-  return dynDB.getAsync(params)
+  const dynDB   = new aws.DynamoDB.DocumentClient();
+  return dynDB.get(params).promise()
   .then(function(d) {
     if (debug) {
       console.info('--## Fetched this config: %j ##--', d.Item);
+    }
+    if ( _.isUndefined(d.Item) ) {
+      throw new Error("Couldn't find config for name: " + p.configName);
     }
     return d.Item;
   });
